6a66cfd1d61b345d722204244a25a369fd3088ec
lenizeramos <lenizemr@gmail.com>
2024-05-14 23:10:08 -0700

    JS code for exercise 5

---
+++ js/exercise5.js
@@ -0,0 +1,27 @@
+var name = "Elisson"
+var mark = 100
+
+if (mark >= 80) {
+    console.log("A")
+} else if (mark >= 60) {
+    console.log("B")
+} else if (mark >= 40) {
+    console.log("C")
+} else {
+    console.log("No Grade")
+}
+    
+
+// var name = "Elisson"
+// var mark = 50
+// var grade = "No grade"
+
+// if (mark >= 80) {
+//     grade = "A"
+// } else if (mark >= 60) {
+//     grade = "B"
+// } else if (mark >= 40) {
+//     grade = "C"
+// }
+    
+// console.log(`Student ${name} grade is: ${grade}`)
