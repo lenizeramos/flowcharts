19c853390a88a4b45a76adcf52aca10c0bc538ac
lenizeramos <lenizemr@gmail.com>
2024-05-14 23:08:45 -0700

    JS code for exercise 3

---
+++ js/exercise3.js
@@ -0,0 +1,16 @@
+var n1 = 1
+var n2 = 5
+var n3 = 4
+
+if (n1 > n2) {
+    if (n1 > n3) {
+        console.log(n1)
+    } else {
+        console.log(n3)
+    }
+} else if (n2 > n3) {
+    console.log(n2)
+
+} else {
+    console.log(n3)
+}
